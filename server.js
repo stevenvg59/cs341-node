6005855a7df30d6b7a2a0f44e68a2324de1487ea
dstevenvg <stevensvg59@gmail.com>
2023-04-22 17:58:23 -0500

    Adding nodemon

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req,res) => {
+  res.send("Sarah Birch");
+});
+const port = 3000;
+
+app.listen(process.env.port || port);
+console.log('Web Server is listening at port '+ (process.env.port || port));
