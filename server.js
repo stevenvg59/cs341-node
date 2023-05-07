7b5066edd9eae4ab756860939f8af1051de03cca
dstevenvg <stevensvg59@gmail.com>
2023-05-06 21:32:32 -0500

    .....

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 const express = require('express');
-const app = express();
-
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connect'); 
 
 const port = process.env.PORT || 8080;
+const app = express();
+
 
 app.use(bodyParser.json());
-
+app.use((req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        next();
+      });
 app.use('/', require('./routes'));
 
-mongodb.initDb(() => {
-    
-        app.listen(process.env.port || port);
-        console.log('Connected to DB and listening to port 8080');
-    
+mongodb.initDb((err, mongodb) => {
+        if (err) {
+          console.log(err);
+        } else {
+          app.listen(port);
+          console.log(`Connected to DB and listening on ${port}`);
+        }
 });
