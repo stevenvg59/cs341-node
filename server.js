3bf7a7b7c1a5845c26ef2ce657ef684da7f8bb6b
dstevenvg <stevensvg59@gmail.com>
2023-05-06 21:36:30 -0500

    Adding Procfile

--- server.js
+++ server.js
@@ -6,12 +6,13 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 
-app.use(bodyParser.json());
-app.use((req, res, next) => {
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        next();
-      });
-app.use('/', require('./routes'));
+app
+        .use(bodyParser.json())
+        .use((req, res, next) => {
+                res.setHeader('Access-Control-Allow-Origin', '*');
+                next();
+        })
+        .use('/', require('./routes'));
 
 mongodb.initDb((err, mongodb) => {
         if (err) {
