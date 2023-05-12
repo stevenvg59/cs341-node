f30f76ace362dfa6ecd5a729a730b752bf690df4
dstevenvg <stevensvg59@gmail.com>
2023-05-11 23:03:13 -0500

    New changes for avoiding empty list

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -2,6 +2,19 @@ const mongodb = require('../db/connect');
 
 const ObjectId = require('mongodb').ObjectId;
 
+const getOne = async (req, res, next) => {
+  const userId = new ObjectId(req.params.id);
+  const result = await mongodb
+  .getDb()
+  .db()
+  .collection('contacts')
+  .find({ _id: userId });
+  result.toArray().then((lists) => {
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).json(lists[0]);
+});
+};
+
 const getAll = async (req, res, next) => {
     const result  = await mongodb
     .getDb()
@@ -14,17 +27,5 @@ const getAll = async (req, res, next) => {
       });
 };
 
-const getOne = async (req, res, next) => {
-    const userId = new ObjectId(req.params.id);
-    const result = await mongodb
-    .getDb()
-    .db()
-    .collection('contacts')
-    .find({ _id: userId });
-    result.toArray().then((lists) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(lists[0]);
-  });
-};
 
 module.exports = { getAll, getOne };
