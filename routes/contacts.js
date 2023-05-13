3f124d266e951431e267f87fa55e07d48a0085ea
dstevenvg <stevensvg59@gmail.com>
2023-05-13 09:45:21 -0500

    lesson3 changes

--- routes/contacts.js
+++ routes/contacts.js
@@ -7,4 +7,10 @@ router.get('/', contactsController.getAll);
 
 router.get('/:id', contactsController.getOne);
 
+router.post('/', contactsController.createContact);
+
+router.put('/:id', contactsController.updateContact);
+
+router.delete('/:id', contactsController.deleteContact);
+
 module.exports = router;
